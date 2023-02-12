91997610e3c9f681e893646071fc0289871000f3
Purposelessness <yagodarov.misha.2003@mail.ru>
2023-02-12 18:34:21 +0300

    feat: processing special prime numbers (2^r - 1)

--- backtrack/backtrack.h
+++ backtrack/backtrack.h
@@ -121,24 +121,85 @@ bool FindCoords(int y) {
   return false;
 }
 
+struct Pair {
+  int num = 1;
+  int degree = 0;
+};
+
+int IsPrimeOfTwo(int size) {
+  Pair primes[] = {{3, 2}, {7, 3}, {31, 5}};
+  for (auto& prime : primes) {
+    if (size == prime.num) {
+      return prime.degree;
+    }
+  }
+  return -1;
+}
+
+bool IsPrime(int c) {
+  int primes[] = {3, 5, 7, 11, 13, 17, 23, 29, 31, 37};
+  for (int num : primes) {
+    if (c == num) {
+      return true;
+    }
+  }
+  return false;
+}
+
+void PrimeTwoSearch() {
+  answer[0].x = 1;
+  answer[0].y = 0;
+  int size = (n + 1) / 2;
+  answer[0].size = size;
+  answer[1].size = answer[2].size = size - 1;
+  answer[1].x = size + 1;
+  answer[2].x = 1;
+  answer[2].y = size;
+  answer_size = 3;
+  int t = n;
+  while (size > 1) {
+    size /= 2;
+    answer[answer_size].size = answer[answer_size + 1].size =
+        answer[answer_size + 2].size = size;
+    t -= size;
+    answer[answer_size].x = answer[answer_size].y = t + 1;
+    answer[answer_size + 1].x = t + 1;
+    answer[answer_size + 1].y = t - size;
+    answer[answer_size + 2].x = t - size + 1;
+    answer[answer_size + 2].y = t;
+    answer_size += 3;
+  }
+}
+
 void Backtrack(int n_new) {
-  // Finding square sizes
   n = n_new;
-  max_square_size = 0.7 * n;
-  second_max_square_size = 0.5 * n;
-  levels = new int[n + 1]{};
   max_square_count = 6 * std::log2(3 * n - 1) - 9;
-  partial_answer = new Square[max_square_count]{};
   answer = new Square[max_square_count]{};
-  levels[0] = n;
-  BacktrackImpl(0, max_square_size);
-  delete[] levels;
-  delete[] partial_answer;
-  // Finding coords
-  answer_left = answer_size;
-  x_max = 40 - n;
-  rect = new std::bitset<40>[n];
-  FindCoords(0);
+
+  auto deg = IsPrimeOfTwo(n_new);
+  if (deg != -1) {
+    PrimeTwoSearch();
+  } else {
+    // Finding square sizes
+    if (IsPrime(n)) {
+      max_square_size = 0.57 * n;
+    } else {
+      max_square_size = 0.75 * n;
+    }
+    second_max_square_size = 0.5 * n;
+    partial_answer = new Square[max_square_count]{};
+    levels = new int[n + 1]{};
+    levels[0] = n;
+    BacktrackImpl(0, max_square_size);
+    delete[] levels;
+    delete[] partial_answer;
+    // Finding coords
+    answer_left = answer_size;
+    x_max = 40 - n;
+    rect = new std::bitset<40>[n];
+    FindCoords(0);
+    delete[] rect;
+  }
 
   std::cout << answer_size << '\n';
   for (int i = 0; i < answer_size; ++i) {
@@ -146,7 +207,6 @@ void Backtrack(int n_new) {
               << '\n';
   }
 
-  delete[] rect;
   delete[] answer;
 }
 
--- backtrack/main.cc
+++ backtrack/main.cc
@@ -4,9 +4,9 @@
 
 int main() {
   std::cin >> n;
-//  clock_t time = clock();
+  clock_t time = clock();
   Backtrack(n);
-//  std::cout << "Test time: "
-//            << static_cast<double>(clock() - time) / CLOCKS_PER_SEC << '\n';
+  std::cout << "Test time: "
+            << static_cast<double>(clock() - time) / CLOCKS_PER_SEC << '\n';
   return 0;
 }
