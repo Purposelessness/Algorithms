72a9af88352f0ab6b70dff8c063f6a5e87b5ffb5
Purposelessness <yagodarov.misha.2003@mail.ru>
2023-04-09 17:44:58 +0300

    feat: added some path methods for constructing path

--- commis_voyageur/graph.h
+++ commis_voyageur/graph.h
@@ -70,10 +70,78 @@ struct PointHash {
 
 using PointSet = std::unordered_set<Point, PointHash>;
 
+#define _erase(c, x) c.erase(std::remove(c.begin(), c.end(), x), c.end())
+
 struct Path {
+  [[nodiscard]] bool IsConstructed() const {
+    return !data.empty();
+  }
+
+  void TryContinuePath() {
+    int begin = data[0];
+    int end = data[data.size() - 1];
+    while (!edges.empty()) {
+      auto it = std::find_if(
+          edges.begin(), edges.end(),
+          [begin, end](const Point& p) { return p.x == begin || p.y == end; });
+      if (it == edges.end()) {
+        return;
+      }
+      if (it->x == begin) {
+        data.insert(data.begin(), it->y);
+      } else {
+        data.push_back(it->x);
+      }
+      edges.erase(it);
+    }
+  }
+
+  bool TryMakePath() {
+    if (edges.size() < 2) {
+      return false;
+    }
+    for (const auto& e : edges) {
+      auto copy = edges;
+      std::vector<int> path{e.y, e.x};
+      _erase(copy, e);
+      while (!copy.empty()) {
+        int next = path[path.size() - 1];
+        auto it = std::find_if(copy.begin(), copy.end(),
+                               [next](const Point& p) { return p.y == next; });
+        if (it == copy.end()) {
+          break;
+        }
+        path.push_back(it->x);
+        copy.erase(it);
+      }
+      if (copy.empty()) {
+        edges.clear();
+        data = path;
+        return true;
+      }
+    }
+    return false;
+  }
+
+  [[nodiscard]] Point GetCyclePoint() {
+    if (!IsConstructed()) {
+      return {-1, -1};
+    }
+    return {data[data.size() - 1], data[0]};
+  }
+
+  void PushEdge(const Point& edge) {
+    edges.push_back(edge);
+    if (data.empty()) {
+      TryMakePath();
+    } else {
+      TryContinuePath();
+    }
+  }
+
   std::vector<Point> edges;
   std::vector<int> data;
-  int length;
+  int length = 0;
 };
 
 #define _path path.data
