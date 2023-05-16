ec047df689475e7dce92954e26a79518fcda0e98
Purposelessness <yagodarov.misha.2003@mail.ru>
2023-05-16 21:03:56 +0300

    refactor: continued

--- algorithms.h
+++ algorithms.h
@@ -1,9 +1,9 @@
 #ifndef ALGORITHMS_H_
 #define ALGORITHMS_H_
 
-#include "aho_corasick.h"
-#include "blum_floyd_pratt_rivest_tarjan.h"
-#include "knuth_morris_pratt.h"
-#include "quickselect.h"
+#include "modules/aho_corasick.h"
+#include "modules/blum_floyd_pratt_rivest_tarjan.h"
+#include "modules/knuth_morris_pratt.h"
+#include "modules/quickselect.h"
 
 #endif  // ALGORITHMS_H_
